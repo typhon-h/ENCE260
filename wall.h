b3503e095cdb79dadc9920c9038e4c7a961be9db
ltr35 <ltr35@uclive.ac.nz>
2021-10-08 19:38:39 +1300

    changes to Wall_t struct - boundary_condition

--- wall.h
+++ wall.h
@@ -22,36 +22,37 @@
 #define WEST_WALL_BOUNDARY     0
 
 // Wall movement patterns
-#define UP_COL_WALL(X)      { (X), (0), (EAST) }  //Move wall right
-#define DOWN_COL_WALL(X)    { (X), (4), (WEST) }  //Move wall left
-#define UP_ROW_WALL(X)      { (X), (6), (NORTH) } //Move wall up
-#define DOWN_ROW_WALL(X)    { (X), (0), (SOUTH) } //Move wall down
+#define UP_COL_WALL(X)      { (X), (0), (EAST), (EAST_WALL_BOUNDARY)}  //Move wall right
+#define DOWN_COL_WALL(X)    { (X), (4), (WEST), (EAST_WALL_BOUNDARY)}  //Move wall left
+#define UP_ROW_WALL(X)      { (X), (6), (NORTH), (SOUTH_WALL_BOUNDARY)} //Move wall up
+#define DOWN_ROW_WALL(X)    { (X), (0), (SOUTH), (SOUTH_WALL_BOUNDARY)} //Move wall down
 
 // Enum for increment direction of walls
 typedef enum
 {
-   NORTH         = -1,
-   EAST          = 1,
-   SOUTH         = 1,
-   WEST          = -1,
+   EAST = 1,
+   SOUTH,
+   WEST,
+   NORTH,
    OUT_OF_BOUNDS = 0
 } direction;
-
+// As they had shaaring values had to change for switch statement
 
 // Type definition for a wall
 typedef struct
 {
-   uint8_t   bit_data; //0b 11000111 -- (1= Non-passible, 0=Passible)
-   uint8_t   pos;      //0, 1, 2, 3, ...
-   direction dir;      // direction is enum, NORTH, EAST, SOUTH, WEST
-} Wall_t;              // Wall object
+   uint8_t   bit_data;    // 0b11000111 -- (1= Non-passible, 0=Passible)
+   uint8_t   pos;         // 0, 1, 2, 3, ...
+   direction dir;         // direction is enum, NORTH, EAST, SOUTH, WEST
+   uint8_t boundary_cond; // If pos>coundary_cond for wall deletion
+} Wall_t;                 // Wall object
 
 
 // Create a wall and spawn it in starting position
 void wall_create(void);
 
 // Toggle wall visibilty on display
-void toggle_wall(Wall_t col_wall, bool display_on);
+void toggle_wall(bool display_on);
 
 // Shift wall in increment direction (contained within the wall)
 void move_wall(void);
