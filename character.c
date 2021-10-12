f92d15e421933f4227aafb4034a89c563ae08509
Lucas <ltr35@uclive.ac.nz>
2021-10-12 22:46:10 +1300

    Changed move_ functions to return bool if on border, add new attribute 'stun'

--- character.c
+++ character.c
@@ -9,13 +9,14 @@
 
 // Character position
 Position_t CHARACTER_POS;
+bool character_stun = false;
 
 
 // Display character at default coordinates
-void character_init()
+void character_init(uint8_t life_count)
 {
    CHARACTER_POS = (Position_t){
-      .x = DEFAULT_X, .y = DEFAULT_Y
+      .x = DEFAULT_X, .y = DEFAULT_Y, .lives = life_count
    };
    display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
 }
@@ -27,7 +28,16 @@ void character_disable()
    display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, false);
 }
 
+// Implements stun attribute in the game (after wall collsion)
+void toggle_stun(bool stun_set)
+{
+	character_stun = stun_set;
+}
 
+bool get_stun_condition()
+{
+	return character_stun;
+}
 /** Return current character coordinates
  *  @return Position_t with .x .y values */
 Position_t get_character_pos(void)
@@ -37,50 +47,58 @@ Position_t get_character_pos(void)
 
 
 /** Move character 1 unit west*/
-void move_west()
+bool move_west()
 {
    if (WEST_CHARACTER_BOUNDARY < CHARACTER_POS.x)
    {
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, false);
       CHARACTER_POS.x -= 1;
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
+      return 0;
    }
+   return 1;
 }
 
 
 /** Move character 1 unit east*/
-void move_east()
+bool move_east()
 {
    if (EAST_CHARACTER_BOUNDARY > CHARACTER_POS.x)
    {
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, false);
       CHARACTER_POS.x += 1;
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
+      return 0;
    }
+	return 1;
 }
 
 
 /** Move character 1 unit north*/
-void move_north()
+bool move_north()
 {
    if (NORTH_CHARACTER_BOUNDARY < CHARACTER_POS.y)
    {
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, false);
       CHARACTER_POS.y -= 1;
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
+      return 0;
    }
+   	return 1;
 }
 
 
 /** Move character 1 unit south*/
-void move_south()
+bool move_south()
 {
    if (SOUTH_CHARACTER_BOUNDARY > CHARACTER_POS.y)
    {
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, false);
       CHARACTER_POS.y += 1;
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
+      return 0;
    }
+   return 1;
 }
 
 
@@ -96,20 +114,30 @@ void character_update()
       display_pixel_set(CHARACTER_POS.x, CHARACTER_POS.y, true);
    }
    //Move character in direction of navswitch input
-   if (navswitch_push_event_p(NAVSWITCH_NORTH))
+   if (!character_stun) 
    {
-      move_north();
+	   if (navswitch_push_event_p(NAVSWITCH_NORTH))
+	   {
+		  move_north();
+	   }
+	   else if (navswitch_push_event_p(NAVSWITCH_SOUTH))
+	   {
+		  move_south();
+	   }
+	   else if (navswitch_push_event_p(NAVSWITCH_EAST))
+	   {
+		  move_east();
+	   }
+	   else if (navswitch_push_event_p(NAVSWITCH_WEST))
+	   {
+		  move_west();
+	   }
    }
-   else if (navswitch_push_event_p(NAVSWITCH_SOUTH))
-   {
-      move_south();
-   }
-   else if (navswitch_push_event_p(NAVSWITCH_EAST))
-   {
-      move_east();
-   }
-   else if (navswitch_push_event_p(NAVSWITCH_WEST))
-   {
-      move_west();
-   }
+}
+
+/* Returns high if CHARACTER_POS has zero lives */
+bool decrease_player_lives()
+{
+	CHARACTER_POS.lives--;
+	return(CHARACTER_POS.lives == 0);
 }
