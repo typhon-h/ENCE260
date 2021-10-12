1743ed67a10eae19ec17ff623d520eaf99c45aea
Lucas <ltr35@uclive.ac.nz>
2021-10-12 22:59:28 +1300

    as per changes to character.c, given thge struct 'lives' type

--- character.h
+++ character.h
@@ -22,10 +22,17 @@
 typedef struct
 {
    uint8_t x, y;
+   uint8_t lives;
 } Position_t;
 
 //Create the character at the default starting position
-void character_init(void);
+void character_init(uint8_t life_count);
+
+// Toggles the stun state of the player
+void toggle_stun(bool stun_set);
+
+// Outputs the current stun state
+bool get_stun_condition(void);
 
 //Turn off character
 void character_disable(void);
@@ -33,13 +40,17 @@ void character_disable(void);
 //Return current position of the character
 Position_t get_character_pos(void);
 
-//Move the character in the given direction, restricted by boundary
-void move_west(void);
-void move_east(void);
-void move_north(void);
-void move_south(void);
+// Move the character in the given direction, restricted by boundary
+// Returns true if character is unable to move (For Push Wall gamemode)
+bool move_west(void);
+bool move_east(void);
+bool move_north(void);
+bool move_south(void);
 
 //Poll navswitch input and update character position
 void character_update(void);
+
+// Reduce player lives by one, returns true if game has ended
+bool decrease_player_lives(void);
 
 #endif
