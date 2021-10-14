4c6bdd8f77019855e4e99790cf72437e5c986917
Harrison Tyson <hty13@uclive.ac.nz>
2021-10-15 01:27:03 +1300

    Removed unused BPM_change function

--- sound.c
+++ sound.c
@@ -5,20 +5,20 @@
 #include "sound.h"
 
 //Extracted from ../apps/space12.c
-#define PIEZO1_PIO           PIO_DEFINE(PORT_D, 4) //Pin 1
-#define PIEZO2_PIO           PIO_DEFINE(PORT_D, 6) //Pin 3
+#define PIEZO1_PIO    PIO_DEFINE(PORT_D, 4)        //Pin 1
+#define PIEZO2_PIO    PIO_DEFINE(PORT_D, 6)        //Pin 3
 
 // Need to get rid of this magic number = TWEETER_TASK_RATE
-static tweeter_scale_t scale_table[] = TWEETER_SCALE_TABLE (5000);
-static tweeter_t     tweeter;
-static mmelody_t     melody;
-static mmelody_obj_t melody_info;
-static tweeter_obj_t tweeter_info;
+static tweeter_scale_t scale_table[] = TWEETER_SCALE_TABLE(5000);
+static tweeter_t       tweeter;
+static mmelody_t       melody;
+static mmelody_obj_t   melody_info;
+static tweeter_obj_t   tweeter_info;
 
 // Activate speaker
 void toggle_speaker(bool state)
 {
-    //Implements dual pin push/pull polling
+   //Implements dual pin push/pull polling
    pio_output_set(PIEZO1_PIO, state);
    pio_output_set(PIEZO2_PIO, !state);
 }
@@ -46,23 +46,20 @@ void sound_stop() // Stop playing current sound
 
 void sound_play(char song[]) //Play song
 {
-    mmelody_play(melody, song);
+   mmelody_play(melody, song);
 }
+
 
 void beep() // Single beep used for testing
 {
-    mmelody_play(melody, "C,C+,C");
+   mmelody_play(melody, "C,C+,C");
 }
 
-void BPM_change(uint16_t tune_bpm_rate) // Update BPM of melody
-{
-	mmelody_speed_set(melody, tune_bpm_rate);
-}
 
 // Initialize sound drivers
 void sound_init(uint16_t tweeter_task_rate, uint16_t tune_task_rate, uint16_t tune_bpm_rate)
 {
-    // Speaker object init
+   // Speaker object init
    tweeter = tweeter_init(&tweeter_info, tweeter_task_rate, scale_table);
 
    //Speaker pins init
