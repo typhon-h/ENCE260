541d0024ecbaa2c47c707b3b84fb1fc5174139a0
Lucas <ltr35@uclive.ac.nz>
2021-10-14 23:07:29 +1300

    as per changes to sound.c

--- sound.h
+++ sound.h
@@ -22,6 +22,9 @@ void sound_play(char song[]);
 // Single beep used for testing
 void beep(void);
 
+// Changes the BPM of a song
+void BPM_change(uint16_t tune_bpm_rate);
+
 // Initialize sound drivers
 void sound_init(uint16_t tweeter_task_rate, uint16_t tune_task_rate, uint16_t tune_bpm_rate);
 #endif
