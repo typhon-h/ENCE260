e5119841fdeba389019d4bff04865d5fc349ed0f
Lucas <ltr35@uclive.ac.nz>
2021-10-17 23:52:29 +1300

    Minor change

--- game_manager.h
+++ game_manager.h
@@ -17,7 +17,7 @@
 #define END_PROMPT             " GAME OVER SCORE:" //Additional whitespace to insert score
 #define END_PROMPT_LEN         17
 #define SIZE_OF_UINT8          8                   //For buffer on end message for score
-#define MENU_TONE              "A"
+#define MENU_TONE              "A,A#,"
 // Menu text for each gamemode (for displaying)
 #define HARD_MODE_TEXT         " HARDMODE "
 #define THREE_LIVES_TEXT       " THREE LIVES "
@@ -45,7 +45,6 @@ typedef enum
 } GAMESTATES_t;
 
 
-
 /*  Initialize game manager, LED and starts game menu
  *  @param message_rate: text scroll speed of MENU texts
  *  @brief: using 3x5 font, displays " SELECT GAME MODE "
@@ -61,48 +60,56 @@ bool get_game_state(void);
 
 
 /*  Returns the pause state of the game
- *  @return true if the game is paused else false */
+ *  @return true if the game is paused else false
+ */
 bool get_pause_state(void);
 
 
 /*  Checks for button input to pause game
  *  @brief: button only pauses the game if it's active.
- *          MENU_TONE is played during paused state */
+ *          MENU_TONE is played during paused state
+ */
 void check_pause_button(void);
 
 
 /*  Updates game states
  *  @brief: checks navswitch to scroll through menu (if in menu).
- *          checks for button push to either redo game or select gamemode. */
+ *          checks for button push to either redo game or select gamemode.
+ */
 void game_state_update(void);
 
 
 /*  Initialize game components when starting
  *  @brief: gives player lives depending on game mode
- *          initialises all modules used in game */
+ *          initialises all modules used in game 
+ */
 void game_start(void);
 
 
 /*  Outlines process of a game_ending (text display, music played)
-*  @brief: Displays score and plays ending music END_GAME_MUSIC */
+*  @brief: Displays score and plays ending music END_GAME_MUSIC
+*/
 void game_outro(void);
 
 
 /*  Checks to see if a "collision" has occured
  *  @brief: As mentioned, stun was introduced so player doesn't have multiple loss of lives from single collision
  *		Function only returns true if collsion & not stunned player cannot move whilst stunned
- *		(stun is removed after wall moves away, prevent player moving into another part of wall) */
+ *		(stun is removed after wall moves away, prevent player moving into another part of wall)
+ */
 void check_collisions(void);
 
 
 /*  Decreases player lives
  *  @brief: decrease_character_lives() decreases lives and return true is lives = 0
- *          if lives = 0, game enters ending state */
+ *          if lives = 0, game enters ending state
+ */
 void decrease_lives(void);
 
 
 /*  Increments game score
- *  @brief: as there is a single moving wall, this is called ervery time the wall resets */
+ *  @brief: as there is a single moving wall, this is called ervery time the wall resets 
+ */
 void increment_score(void);
 
 
